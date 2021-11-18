2441d1995f61bb10f9a93b3a2acc71805f28522a
AlejandrVilla <avillah@unsa.edu.pe>
2021-11-18 17:55:51 -0500

    sube mrd samuel XD

--- binomial heap/bheap.h
+++ binomial heap/bheap.h
@@ -3,7 +3,6 @@
 #include "nodob.h"
 #include <list>
 #include <iostream>
-#include <fstream>
 #include <vector>
 
 template <class T>
@@ -14,39 +13,36 @@ private:
     NodoB<T> *m_Min;
 
 private:
-    NodoB<T> *Unir(NodoB<T> *p, NodoB<T> *q); // falta julio
-    void Compactar();                         // falta alejandro
+    NodoB<T> *Unir(NodoB<T> *p, NodoB<T> *q);       // falta julio
+    void Compactar();                               // falta alejandro
 
 public:
-    BHeaps() {}
+    BHeaps(){}
     void Insert(T d);                       // O(log(n))
     void Delete(NodoB<T> *e);               // O(log(n))
     void Decrease_key(NodoB<T> *&p, T val); // O(log(n))
     void Extrac_Min();                      // O(log(n))
-    void recorrer(std::ostream &, NodoB<T> *);
-    void Show_Dot(std::ostream&);                // falta samuel
-    void Save_Dot(std::string);
-    T GetMin();                             // O(1)
+    void Show_Dot();                                // falta samuel
+    T GetMin(); // O(1)
 
     void print();
 };
 
 //------------------------------
 template <class T>
-NodoB<T> *BHeaps<T>::Unir(NodoB<T> *p, NodoB<T> *q)
+NodoB<T>* BHeaps<T>::Unir(NodoB<T> *p, NodoB<T> *q)
 {
     // Unir dos arboles binomiales de grado k-1, en un arbol binomial de grado k
     // Asegurar que el puntero al padre se actualize
-    if (q->m_Dato > p->m_Dato)
-    {
-        q->m_Padre = p;
-        (p->m_pSons).push_front(q);
-        p->m_Grado++;
-        return p;
+    if(q -> m_Dato > p -> m_Dato) {
+		q -> m_Padre = p;
+		(p -> m_pSons).push_front(q);
+		p -> m_Grado ++;
+		return p;
     }
-    p->m_Padre = q;
-    (q->m_pSons).push_front(p);
-    q->m_Grado++;
+    p -> m_Padre = q;
+    (q -> m_pSons).push_front(p);
+    q -> m_Grado ++;
     return q;
 }
 
@@ -56,35 +52,35 @@ void BHeaps<T>::Compactar()
     // Verificar que todos los nodos de la raiz tengan un grado distinto.
     // En caso tengan el mimo grado, entonces llama  a Unir
     // Colocar el puntero al menor en el menor de los nodos raiz
-    if (m_Roots.size() == 1)
+    if( m_Roots.size() == 1 )
         m_Min = *(m_Roots.begin());
-    else if (m_Roots.size() > 1)
+    else if( m_Roots.size() > 1 )
     {
-        typename std::list<NodoB<T> *>::iterator last = --(m_Roots.end());
-        std::vector<NodoB<T> *> grades((*last)->m_Grado + 2, nullptr);
+        typename std::list<NodoB<T>* >::iterator last = --(m_Roots.end());
+        std::vector<NodoB<T>* > grades((*last)->m_Grado+2,nullptr);
 
-        typename std::list<NodoB<T> *>::iterator p = m_Roots.begin();
-        for (; p != m_Roots.end(); ++p)
+        typename std::list<NodoB<T>* >::iterator p = m_Roots.begin();
+        for( ; p!=m_Roots.end() ; ++p )
         {
-            if (grades[(*p)->m_Grado] == nullptr)
+            if( grades[(*p)->m_Grado] == nullptr )
                 grades[(*p)->m_Grado] = *p;
             else
             {
-                while (grades[(*p)->m_Grado] != nullptr)
+                while( grades[(*p)->m_Grado] != nullptr )
                 {
-                    NodoB<T> *q = grades[(*p)->m_Grado];
+                    NodoB<T>* q = grades[(*p)->m_Grado];
                     grades[(*p)->m_Grado] = 0;
-                    NodoB<T> *u = Unir(*p, q);
+                    NodoB<T>* u = Unir(*p,q);
                     m_Roots.remove(q);
                     *p = u;
                 }
                 grades[(*p)->m_Grado] = *p;
             }
         }
-        typename std::list<NodoB<T> *>::iterator it = m_Roots.begin();
+        typename std::list<NodoB<T>* >::iterator it = m_Roots.begin();
         m_Min = *it;
-        for (; it != m_Roots.end(); ++it)
-            if ((*it)->m_Grado < m_Min->m_Grado)
+        for( ; it!=m_Roots.end() ; ++it )
+            if( (*it)->m_Grado < m_Min->m_Grado )
                 m_Min = *it;
     }
 }
@@ -96,7 +92,7 @@ void BHeaps<T>::Insert(T d)
     NodoB<T> *pNew = new NodoB<T>(d); // O(1)
     m_Roots.push_front(pNew);         // O(1)
     Compactar();                      // O(log(n))
-    std::cout << "Se inserto\n";
+    std::cout<<"Se inserto\n";
 }
 
 template <class T>
@@ -118,7 +114,7 @@ void BHeaps<T>::Decrease_key(NodoB<T> *&p, T val)
     } // O(log(n))
 
     // Nota: En caso que val sea menor que el mínimo, actualizar el puntero al mínimo
-    if (p->m_Grado < m_Min->m_Grado)
+    if( p->m_Grado < m_Min->m_Grado )
         m_Min = p;
 }
 
@@ -141,23 +137,18 @@ void BHeaps<T>::Extrac_Min()
 }
 
 template <class T>
-void BHeaps<T>::recorrer(std::ostream &os, NodoB<T> *raiz)
+void BHeaps<T>::Show_Dot()
 {
-    os << node->m_Dato << "[color = white fontcolor = white label = \"<f0> | {{<f1> " << node->m_Dato << "} | FE=" << node->m_Grado << "} | <f2> \" style = filled fillcolor = \"#AE2115\" ]";
-    for (NodoB<T> *node : raiz->m_pSons)
-    {
-        os << raiz->m_Dato << ":f0 -> " << node->m_Dato << ":f1 [color = \"#AE2115\"];\n";
-        recorrer(os, node);
-    }
+    std::cout << "Make your code here" << '\n';
 }
 
-template <class T>
+template<class T>
 void BHeaps<T>::print()
 {
-    typename std::list<NodoB<T> *>::iterator it = m_Roots.begin();
-    for (; it != m_Roots.end(); ++it)
-        std::cout << (*it)->m_Dato << ' ';
-    std::cout << '\n';
+    typename std::list<NodoB<T>* >::iterator it = m_Roots.begin();
+    for(;it!=m_Roots.end();++it)
+        std::cout<<(*it)->m_Dato<<' ';
+    std::cout<<'\n';
 }
 
 #endif
